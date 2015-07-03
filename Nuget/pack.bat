3aeb7c431fbc9629c0f088c9f801f24a466bc76f
David Knipe <david.knipe@episerver.com>
2015-07-03 17:46:19 +0100

    v2.1 release
    Ensures the "To View Mode" link (globe icon) works the same when the
    menu is pinned as for the slide down menu

--- Nuget/pack.bat
+++ Nuget/pack.bat
@@ -1,12 +1,15 @@
 @Echo Setting up folder structure
 md Package\lib\net45\
 md Package\tools\
+md Package\content\ClientResources\Scripts\MenuPin\
 
 @Echo Removing old files
 del /Q Package\lib\net45\*.*
 
 @Echo Copying new files
 copy ..\MenuPin\bin\Release\MenuPin.dll Package\lib\net45 
+copy ..\MenuPin\ClientResources\Scripts\MenuPin\MenuPinInit.js Package\content\ClientResources\Scripts\MenuPin\MenuPinInit.js
+copy ..\MenuPin\module.config.transform Package\content
 
 @Echo Packing files
 "..\.nuget\nuget.exe" pack package\MenuPin.nuspec
