e102e41ffe7aaa43360051ed4b46291d9fd489a0
David Knipe <david.knipe@episerver.com>
2016-04-26 13:56:18 +0100

    v3.3 Commit
    Adds compatibility with Episerver Commerce UI

---
+++ MenuPin/MenuProvider/AddCommercePinToMenu.cs
@@ -0,0 +1,22 @@
+﻿using EPiServer.ServiceLocation;
+using EPiServer.Shell.Navigation;
+using System.Collections.Generic;
+
+namespace MenuPin.MenuProvider
+{
+    [MenuProvider]
+    public class AddCommercePinToMenu : IMenuProvider
+    {
+        public IEnumerable<MenuItem> GetMenuItems()
+        {
+            var pinMenuItemCommerce = new UrlMenuItem(
+                            "<span class=\"epi-iconPin menuPinButton\"></span>", 
+                            "/global/commerce/catalog",
+                            "javascript: return;"
+                            );
+            pinMenuItemCommerce.Alignment = MenuItemAlignment.Right;
+
+            return new List<MenuItem>() { pinMenuItemCommerce }; 
+        }
+    }
+}
--- MenuPin/MenuProvider/AddPinToMenu.cs
+++ MenuPin/MenuProvider/AddPinToMenu.cs
@@ -7,24 +7,16 @@ namespace MenuPin.MenuProvider
     [MenuProvider]
     public class AddPinToMenu : IMenuProvider
     {
-        private IServiceLocator _serviceLocator;
-        public AddPinToMenu(IServiceLocator serviceLocator)
-        {
-            _serviceLocator = serviceLocator;
-        }
-
         public IEnumerable<MenuItem> GetMenuItems()
         {
-            var uiUrl = EPiServer.Configuration.Settings.Instance.UIUrl.OriginalString.TrimStart("~".ToCharArray()).TrimEnd("/".ToCharArray());
-
-            var pinMenuItemCMS = new UrlMenuItem(
-                            "<span class=\"epi-iconPin\" id=\"menuPin\"></span>", 
+            var pinMenuItemCms = new UrlMenuItem(
+                            "<span class=\"epi-iconPin menuPinButton\"></span>", 
                             "/global/cms/edit",
                             "javascript: return;"
                             );
-            pinMenuItemCMS.Alignment = MenuItemAlignment.Right;
+            pinMenuItemCms.Alignment = MenuItemAlignment.Right;
 
-            return new List<MenuItem>() { pinMenuItemCMS };
+            return new List<MenuItem>() { pinMenuItemCms };
         }
     }
 }
